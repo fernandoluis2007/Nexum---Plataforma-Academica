b5a474b690bae04fb717c6c957125cd9ff40ded0
Luis Fernando Dev <103805607+fernandoluis2007@users.noreply.github.com>
2024-10-22 08:18:03 -0300

    Add files via upload

---
+++ TCC__testes/Agenda/lib/html/footer.php
@@ -0,0 +1,5 @@
+    
+        <script src="<?php echo DIRPAGE.'TCC__testes/Agenda/lib/js/FullCalendar/main.min.js'; ?>"></script>
+        <script src="<?php echo DIRPAGE.'TCC__testes/Agenda/lib/js/javascript.js'; ?>"></script>
+</body>
+</html>
